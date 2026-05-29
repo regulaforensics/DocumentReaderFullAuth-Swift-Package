7eaf552074488df118cc9605c8f7be464692ea6c
Jenkins <jenkins@regula.by>
2026-05-29 21:01:23 +0300

    Release 9.5.18175

--- Package.swift
+++ Package.swift
@@ -10,6 +10,6 @@ let package = Package(
             targets: ["FullAuth"]),
     ],
     targets: [
-        .binaryTarget(name: "FullAuth", url: "https://pods.regulaforensics.com/FullAuth/9.4.17647/DocumentReaderCore_fullauth_9.4.17647.zip", checksum: "4b0970f50bfa08724b430f8cec851c067cd233f4c4dbd47a54bd005b57f56f3b"),
+        .binaryTarget(name: "FullAuth", url: "https://pods.regulaforensics.com/FullAuth/9.5.18175/DocumentReaderCore_fullauth_9.5.18175.zip", checksum: "6c10eed137c9060abef76ce125775647f8e796b91fa49a094d8d3329bbaa1e18"),
     ]
 )
